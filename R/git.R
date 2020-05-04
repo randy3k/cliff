79b9ba27830596d365d6356a28574e1fa5dc72e4
Randy Lai <randy.cs.lai@gmail.com>
2020-05-03 23:55:59 -0700

    added some examples

--- R/git.R
+++ R/git.R
@@ -20,13 +20,49 @@ get_git_path  <- function() {
 
 #' Running a Git Command
 #' @export
-#' @param ... the arguments pass to git command
+#' @param ... the arguments pass to git command, supports the
+#'     [big bang](https://rlang.r-lib.org/reference/nse-force.html) operator `!!!`
 #' @param input text pass to stdin
 #' @param working_dir working directory
 #' @param throw_on_stderr throw an error if git command executed successfully but
 #'     stderr is not empty
 #' @param env additional environment variable pass to git
 #' @param just_the_proc return the underlying process object instead of the output
+#' @return a scalar character that may contains a trailing newline. Use `as.character()`
+#'     to ensure the trailing newline is trimmed.
+#' @details
+#' If `git()` fails to locate the git binary automatically, the path to it could be
+#' specify in `options(git_path = "/path/to/git")`.
+#'
+#' @examples
+#' \donttest{
+#' td <- file.path(tempdir(), "git-examples")
+#' dir.create(td, showWarnings = FALSE)
+#' setwd(td)
+#'
+#' git("init")
+#'
+#' cat("hello\n", file = "hello.txt")
+#'
+#' git("status")
+#'
+#' git("add", "hello.txt")
+#'
+#' git("commit", "-m", "initial message")
+#'
+#' git("log")
+#'
+#' cat("world\n", file = "world.txt")
+#'
+#' git("add", "world.txt")
+#'
+#' git("commit", "-F", "-", input = "second message")
+#'
+#' (sha <- git("rev-parse", "HEAD"))
+#'
+#' git("show", sha)
+#' }
+#'
 git <- function(
         ...,
         input = NULL,
@@ -40,7 +76,7 @@ git <- function(
 
     ellipsis::check_dots_unnamed()
     git_path <- get_git_path()
-    args <- as.character(rlang::list2(...))
+    args <- vapply(rlang::list2(...), function(x) as.character(x), character(1))
     p <- processx::process$new(
         git_path, args, stdin = "|", stdout = "|", stderr = "|", env = env, wd = working_dir)
     if (!is.null(input)) {
